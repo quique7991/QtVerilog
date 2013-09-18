ae2c4d7c0bebaeb8b8bbbbe6ccd1888a45ea8f6d
enjosaureza <enrique.saurez@ucr.ac.cr>
2013-09-17 18:28:06 -0600

    Core Comments

--- GUI/core.cpp
+++ GUI/core.cpp
@@ -3,8 +3,12 @@
 #include <QPainter>
 #include <QMenu>
 
+/*
+    Constructor
+*/
 Core::Core(int index, int left,int top,  QString direccionImagen,QColor color,int cantidadEntradas,int cantidadSalidas,Main *parent,bool force,int width,int height){
-   minHeight = 150;
+    ///Se define los tamaños del anocho y el largo, a partir de la cantidad de netradas y salidas.
+    minHeight = 150;
     if(cantidadEntradas>cantidadSalidas){
         minHeight = cantidadEntradas*10+8;
     }
@@ -14,25 +18,36 @@ Core::Core(int index, int left,int top,  QString direccionImagen,QColor color,in
     if(minHeight<100){
         minHeight=100;
     }
-    if(force){
+    minWidth=100;///Luego es necsario comparar la altura contra la imágen que va adentro.
+    if(force){///Si se envia force se asignan directamente estas variables
         minHeight=height;
         minWidth=width;
     }
     z = qrand()%256;
-    minWidth=100;///Luego es necsario comparar la altura contra la imágen que va adentro.
+    ///Se asignan los valores a los parámetros
     this->left=left;
     this->top=top;
     this->direccionImagen=direccionImagen;
     this->color = color;
     this->parent=parent;
     this->index=index;
+    ///Se asigna la variable que permite mover al objeto.
     this->setFlag(QGraphicsItem::ItemIsMovable);
 }
 
+
+
+/*
+  Función que devuelve el índice del objeto.
+*/
 int Core::getIndex(){
     return this->index;
 }
 
+
+/*
+  Funciones de forma del objeto.
+*/
 QRectF Core::boundingRect() const{
     return QRectF(left,top,minWidth,minHeight);
 }
@@ -43,33 +58,44 @@ QPainterPath Core::shape() const{
     return path;
 }
 
+/*
+  Función que se encarga de dibujar la imagen.
+*/
 void Core::paint(QPainter *painter, const QStyleOptionGraphicsItem *item, QWidget *widget){
+    //Se indica que los inputs no son utilizados.
     Q_UNUSED(widget);
     Q_UNUSED(item);
     //painter->setRenderHint(QPainter::Antialiasing);
-
+    ///Se asigna el color del Brusy y el pen.
     painter->setBrush( QColor(z,z,z) );
     painter->setPen( QPen(color) );
+    ///En caso de que se quiera sustituir la imagen por un rectangulo
     //painter->drawLine(QPointF(1,1),QPointF(2,2));
     //painter->drawRect(QRectF(left,top,minWidth,minHeight));
+    ///Se indica la figura que se va a dibujar y se imprime en la ventana
     QPixmap *pix = new QPixmap(direccionImagen);
     painter->drawPixmap(left,top,minWidth,minHeight,*pix);
 }
 
+/*
+    Función que permite hacer click derecho sobre la imagen para borrarla o hacer un análisis jerárquico.
+*/
 void Core::mousePressEvent(QGraphicsSceneMouseEvent *event){
     QGraphicsItem::mousePressEvent(event);
-    if(event->button()==Qt::RightButton){
-       QMenu* contextMenu = new QMenu ();
+    if(event->button()==Qt::RightButton){///Se verifica si es un click derecho.
+       QMenu* contextMenu = new QMenu ();///Se crea un menu de contexto.
         Q_CHECK_PTR ( contextMenu );
-        parent->setActualItem(this);
-        contextMenu->addAction ( "Analyse" , parent , SLOT (downHierarchie() ));
-        contextMenu->addAction ( "Erase" , parent , SLOT (erase() ));
+        parent->setActualItem(this);///Se el puntero a este objeto en el objeto padre en caso de que se quiera borrar..
+        contextMenu->addAction ( "Analyse" , parent , SLOT (downHierarchie() ));//Se agrega la opción de analizar la jerarquía.
+        contextMenu->addAction ( "Erase" , parent , SLOT (erase() ));///Se borra el objeto.
         contextMenu->popup( QCursor::pos() );
         contextMenu->exec ();
         delete contextMenu;
         contextMenu = 0;
     }
 }
+
+///Funciones no utilizadas que manejan los eventos del mouse
 
 void Core::mouseMoveEvent(QGraphicsSceneMouseEvent *event){
     QGraphicsItem::mouseMoveEvent(event);
--- GUI/core.h
+++ GUI/core.h
@@ -12,22 +12,31 @@ class Main;
 class Core : public QGraphicsItem
 {
 public:
+    ///Constructor.
     Core(int index,int left,int top, QString direccionImagen,QColor color,int cantidadEntradas,int cantidadSalidas,Main *parent,bool force,int width,int height);
+    ///Formas necesario para un QGraphicsItem.
     QRectF boundingRect() const;
     QPainterPath shape() const;
+    ///Funcion de dibujo del objeto.
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *item, QWidget *widget);
+    ///Devuelve el índice en el objeto parent.
     int getIndex();
 
 protected:
+    ///Eventos de mouse
     void mousePressEvent(QGraphicsSceneMouseEvent *event);
     void mouseMoveEvent(QGraphicsSceneMouseEvent *event);
     void mouseReleaseEvent(QGraphicsSceneMouseEvent *event);
 
 private:
+    ///Posicion de la imagen.
     int left,top,minWidth,minHeight,z;
+    ///Direccion de la imagen. Hace falta incluir una default.
     QString direccionImagen;
     QColor color;
+    ///Ventana que contienen a este objeto.
     Main *parent;
+    ///Indice dentro de la lista en el parent.
     int index;
 };
 
